5f4d257e95aae8f73cbc27346c540d3ddd4cf099
Brooks Townsend <brooksmtownsend@gmail.com>
2017-09-14 09:49:44 -0400

    Fixed issue where dragging course to non-container would cause app crash

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -15,6 +15,7 @@ export default class Toolbar extends React.Component {
         </div>
         <div className="Toolbar-item" onClick={scheduleStore.addClass}><span className="Toolbar-text">Add Class</span></div>
         <div className="Toolbar-item" onClick={scheduleStore.addMajor}><span className="Toolbar-text">Add Major</span></div>
+        {/* <div className="Toolbar-item" onClick={scheduleStore.login}><span className="Toolbar-text">Login</span></div> */}
       </div>
     )
   }
