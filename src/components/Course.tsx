4f52043b32b2749c8ea6713f8c7bca98af011818
Brooks Townsend <brooksmtownsend@gmail.com>
2017-10-26 10:05:18 -0400

    Fixed crash for 410, just for a stable release

--- src/components/Course.tsx
+++ src/components/Course.tsx
@@ -25,7 +25,10 @@ export default class Course extends React.Component<{ data: CourseData }, {}> {
 
   constructor(props: { data: CourseData }) {
     super(props)
+    console.log(this.props.data)
+    // this.props.data.geneds = this.props.data.geneds === undefined ? [''] : this.props.data.geneds;
     if (this.props.data.geneds[0] === '') {
+      console.log("we got no gen eds")
       this.props.data.geneds = []
     }
   }
