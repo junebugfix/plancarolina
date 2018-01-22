ee6c055915af5455aab9485d35faf7ff7d4b6cca
Brooks Townsend <brooksmtownsend@gmail.com>
2018-01-22 12:01:14 -0500

    Got testing "working" but haven't gotten testing components functional yet.

---
+++ src/tests/test.tsx
@@ -0,0 +1,14 @@
+'use strict'
+var React = require('react')
+var ReactDOM =  require('react-dom')
+var mocha = require('mocha')
+var expect = require('chai').expect
+
+// import App from '../components/App'
+
+describe("App tests", () => {
+  it('Tests work', () => {
+    let zero = 0
+    expect(0).to.equal(zero)
+  })
+})
