98d47053e79381f0825b004a22a0f1758d787a97
Brooks Townsend <brooksmtownsend@gmail.com>
2018-01-27 17:10:27 -0500

    added some test stuff

--- src/tests/test.jsx
+++ src/tests/test.jsx
@@ -1,14 +1,16 @@
 'use strict'
+
+import App from '../components/App'
+
 var React = require('react')
 var ReactDOM =  require('react-dom')
 var mocha = require('mocha')
 var expect = require('chai').expect
 
-// import App from '../components/App'
-
 describe("App tests", () => {
   it('Tests work', () => {
     let zero = 0
+    let x = <App />
     expect(0).to.equal(zero)
   })
 })
